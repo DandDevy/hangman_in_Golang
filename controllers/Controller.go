7d51acabc3c6f590369896a563c4b0d4c5311b6f
Daniel Ashcroft <dashcroft4@gmail.com>
2019-08-20 10:38:19 +0100

    started making HangMan and Controller.

--- controllers/Controller.go
+++ controllers/Controller.go
@@ -1,1 +1,18 @@
 package controllers
+
+import (
+	"hangman_in_Golang/models"
+	"hangman_in_Golang/views"
+)
+
+type myC struct {
+	hmc views.HangManCli
+	hm models.HangMan
+}
+
+func NewMyC()  {
+	
+}
+func start()  {
+	
+}
--- models/HangMan.go
+++ models/HangMan.go
@@ -1,1 +1,17 @@
 package models
+
+type HangMan struct {
+	wordToGuess         string
+	numAttemptsLeft     int
+	maxNumberOfAttempts int
+
+}
+
+func (hm HangMan) NewHangMan(maxAttempts int) HangMan{
+	//hm.wordToGuess = Controller.GetWordToGuess()
+	//max.. = max
+	//numAtt..lt = max
+
+	return hm
+}
+
